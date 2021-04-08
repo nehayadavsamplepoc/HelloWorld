5c98022a5727b7a4f87d80ee33863f00ed2a8ce9
Kartashov, Alexander <AKartashov@inwk.com>
2021-04-08 09:02:14 +0300

    add entity framework test

---
+++ HelloEntityFramework/UserContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace HelloEntityFramework
+{
+    class UserContext : DbContext
+    {
+        public UserContext() 
+            : base("DbConnection")
+        {}
+
+        public DbSet<User> Users { get; set; }
+    }
+}
